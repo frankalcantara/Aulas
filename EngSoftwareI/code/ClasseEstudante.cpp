d2f4abe06d5d9d5ee52092d193cc61fe1fd0a4d8
Frank de Alcantara <frank.alcantara@gmail.com>
2020-10-20 16:37:44 -0300

    incluindo a classe estudantes

---
+++ EngSoftwareI/code/ClasseEstudante.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+ 
+#define MAX 10
+ 
+class Estudante{
+    private:
+       string  nome;
+       int   matricula;
+       int   nota;
+       float percentualAtingido;
+    public:
+       void setDetalhes(void);
+       void displayDetalhes(void);
+};
+ 
+
+void Estudante::setDetalhes(void){
+    cout << "\033[1;31m"<<"Digite nome: " << "\033[0m" ;
+    cin >> nome;
+    cout <<"\033[1;31m"<< "Digite matricula: "<< "\033[0m" ;
+    cin >> matricula;
+    cout <<"\033[1;31m"<< "Digite nota (máximo 200): "<< "\033[0m" ;
+    cin >> nota;
+     
+    percentualAtingido=(float)nota/200*100;
+}
+ 
+void Estudante::displayDetalhes(void){
+   cout << "\033[1;31m"<< "nome:" << "\033[0m" << nome << 
+   "\033[1;31m" <<"\nMatricula:" << "\033[0m"<< matricula <<"\033[1;31m"<< "\nNotal:" <<  "\033[0m"<< nota << "\033[1;31m"<< "\nPercentual Atingido:"<< "\033[0m"<<percentualAtingido<<"\n\n"<<endl;
+}
+ 
+int main(){
+    
+    Estudante * Estd;
+    Estd = new (nothrow) Estudante[MAX];
+    
+    int n;
+     
+    cout << "\033[1;34m"<<"Quantidade de Estudantes: "<< "\033[0m" ;
+    cin >> n;
+     
+    for(int i=0;i< n; i++){
+        cout << "\033[1;32m"<<"\nDados dos Estudante (" << i+1 << "): \n"<< "\033[0m";
+        Estd[i].setDetalhes();
+    }
+     
+    cout << endl;
+    //para imprimir os dados...
+    for(int i=0;i< n; i++){
+        cout << "\033[1;32m"<<"\nDetalhes do Estudante (" << (i+1) << "):\n";
+        Estd[i].displayDetalhes();
+    }
+     
+    delete[] Estd;
+
+    return 0;
+}
